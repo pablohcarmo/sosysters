2a6e3bd6dbf4b644c12d493de129e1527c9b9dd5
Pablo Carmo <pablohsouzacarmo@gmail.com>
2025-10-05 20:21:14 -0300

    feat: implementa API REST para cadastro de usuárias
    - Adiciona UsuariaController com endpoint POST /usuarias
    - Implementa validação de dados de entrada
    - Configura tratamento de exceções
    - Adiciona resposta padronizada para sucessos e erros

--- src/main/java/br/com/sosysters/controllers/UsuariaController.java
+++ src/main/java/br/com/sosysters/controllers/UsuariaController.java
@@ -3,10 +3,15 @@ package br.com.sosysters.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sosysters.dto.NovaUsuariaDto;
 import br.com.sosysters.dto.UsuariaDto;
 import br.com.sosysters.services.UsuariaService;
 
@@ -20,5 +25,11 @@ public class UsuariaController {
 	public List<UsuariaDto> findAll(){
 		List<UsuariaDto> result = usuariaService.findAll();
 		return result;
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> cadastrarUsuaria(@RequestBody NovaUsuariaDto dto) {
+		usuariaService.cadastrarUsuaria(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 }

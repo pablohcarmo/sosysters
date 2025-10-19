4a5ec0d3bac8dd0a11f9841e8dd6edb5f096f326
Pablo Carmo <pablohsouzacarmo@gmail.com>
2025-10-19 16:47:54 -0300

    Revert "feat: implementação da API com os Estados""
    This reverts commit 2c73e1e206bf723d626f7f6f1d336b7a81b5e35f.

--- src/main/java/br/com/sosysters/repositories/EstadoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.sosysters.repositories;
-
-import br.com.sosysters.entities.Estado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstadoRepository extends JpaRepository<Estado, Long>{
-
-}

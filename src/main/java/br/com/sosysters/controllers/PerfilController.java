b245c7eebae8a683f43a1afa1a58c22cb25f5d2e
Pablo Carmo <pablohsouzacarmo@gmail.com>
2026-05-20 01:03:26 -0300

    feat(perfil): carregar e exibir dados do perfil (controller, template e estilos atualizados)

--- src/main/java/br/com/sosysters/controllers/PerfilController.java
+++ src/main/java/br/com/sosysters/controllers/PerfilController.java
@@ -1,15 +1,24 @@
 package br.com.sosysters.controllers;
 
+import br.com.sosysters.entities.Usuaria;
+import br.com.sosysters.services.UsuariaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/perfil")
 public class PerfilController {
+
+    @Autowired
+    private UsuariaService usuariaService;
     
     @GetMapping
-    public String loadPerfilPage() {
+    public String loadPerfilPage(@AuthenticationPrincipal Usuaria usuaria, Model model) {
+        model.addAttribute("cadastro", usuariaService.carregarMeuCadastro(usuaria.getUsername()));
         return "perfil";
     }
 }

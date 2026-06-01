3bf3cce178baecb079e58b2292af062a50fe93ab
Pablo Carmo <pablohsouzacarmo@gmail.com>
2026-05-31 21:14:25 -0300

    feat(pesquisa): criar página de pesquisa com integração ao layout

--- src/main/java/br/com/sosysters/controllers/PesquisaController.java
+++ src/main/java/br/com/sosysters/controllers/PesquisaController.java
@@ -1,6 +1,9 @@
 package br.com.sosysters.controllers;
 
+import br.com.sosysters.services.SolicitacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/pesquisa")
 public class PesquisaController {
 
+    @Autowired
+    private SolicitacaoService solicitacaoService;
+
     @GetMapping
-    public String loadPesquisaPage() {
+    public String loadPesquisaPage(Model model) {
+        model.addAttribute("solicitacoesPesquisa", solicitacaoService.listarParaPesquisa());
+        model.addAttribute("categoriasPesquisa", solicitacaoService.listarCategoriasPesquisa());
         return "pesquisa";
     }
 }

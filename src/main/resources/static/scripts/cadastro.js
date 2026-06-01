7a338d3e45bc42d8c5a29a845d5cfb6672e5ec9f
Pablo Carmo <pablohsouzacarmo@gmail.com>
2026-06-01 12:30:09 -0300

    fix: usar endpoint relativo no cadastro

--- src/main/resources/static/scripts/cadastro.js
+++ src/main/resources/static/scripts/cadastro.js
@@ -73,7 +73,7 @@ function coletarDadosFormulario() {
 // Função para receber os dados
 async function cadastrarUsuario(dadosUsuario) {
     try {
-        const response = await fetch('http://localhost:8080/usuarias', {
+        const response = await fetch('/usuarias', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(dadosUsuario)
